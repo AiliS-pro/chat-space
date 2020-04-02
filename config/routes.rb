e24ab43a2e86d20a8abbb15a5cfd297e82fd82af
AiliS-pro <chopper.man.015@gmail.com>
2020-04-02 18:36:43 +0900

    インクリメンタルサーチ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :tweets, only: [:index, :create]
+    namespace :api do
+      resources :tweets, only: :index, defaults: { format: 'json' }
+    end
   end
 end
