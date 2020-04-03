c3340803352cfc1692a28bb638f7f94aca640fda
AiliS-pro <chopper.man.015@gmail.com>
2020-04-03 15:59:42 +0900

    chatspace自動更新機能

--- app/assets/javascripts/tweet.js
+++ app/assets/javascripts/tweet.js
@@ -2,7 +2,7 @@ $(function(){
      function buildHTML(tweet){
       if (tweet.image) {
         var html =
-         `<div class="main__message-list__message-unit">
+         `<div class="main__message-list__message-unit" data-tweet-id=${tweet.id}>
             <div class="main__message-list__message-unit__messanger-info">
               <div class="main__message-list__message-unit__messanger-info--messanger-name">
                 ${tweet.user_name}
@@ -21,7 +21,7 @@ $(function(){
         return html;
       } else {
         var html =
-         `<div class="main__message-list__message-unit">
+         `<div class="main__message-list__message-unit" data-tweet-id=${tweet.id}>
             <div class="main__message-list__message-unit__messanger-info">
               <div class="main__message-list__message-unit__messanger-info--messanger-name">
                 ${tweet.user_name}
@@ -39,7 +39,7 @@ $(function(){
         return html;
       };
     }
-$('#new_tweet').on('submit', function(e){
+  $('#new_tweet').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
@@ -61,5 +61,31 @@ $('#new_tweet').on('submit', function(e){
      .fail(function() {
        alert("メッセージ送信に失敗しました");
      });
-})
+  })
+
+  var reloadTweets = function() {
+    var last_tweet_id = $('.main__message-list__message-unit:last').data("tweet-id");
+    $.ajax({
+      url: "api/tweets",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_tweet_id}
+    })
+    .done(function(tweets) {
+      if (tweets.length !== 0) {
+        var insertHTML = '';
+        $.each(tweets, function(i, tweet) {
+          insertHTML += buildHTML(tweet)
+        });
+        $('.tweets').append(insertHTML);
+        $('.main__message-list').animate({ scrollTop: $('.main__message-list')[0].scrollHeight});      
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/tweets/)) {
+    setInterval(reloadTweets, 7000);
+  }
 });
